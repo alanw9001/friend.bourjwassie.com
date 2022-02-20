b7e9354e769d5d47d405c1e32d9f3d60787cf478
Alan <alanw9001@gmail.com>
2022-02-20 00:10:58 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function onClick(e) {
+    e.preventDefault();
+    // get form values
+    let seed = document.getElementById('seed').value;
+    let mood = document.getElementById('mood').value;
+    let randomAvatar = randomAvatarType();
+    let picurl = "https://avatars.dicebear.com/api/" + randomAvatar + "/" + seed + ".svg?mood[]=" + mood;
+    friend = '<img src="' + picurl + '" width="300px" height="300px">';
+    fetch("https://randomuser.me/api/")
+        .then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            friend += "<p>Hi! My name is " + json['results']['name']['first'] + 
+                ", and I'm from " + json.results.location.city + 
+                ". We're going to be best friends!!</p>";
+        });
+    document.getElementById('results').innerHTML = friend;
+}
+
+function randomAvatarType() {
+    const types = ['adventurer', 'pixel-art', 'open-peeps', 'croodles',
+        'big-smile', 'big-ears', 'miniavs', 'bottts', 'micah'];
+    return types[Math.floor(Math.random() * types.length)];
+}
+
+document.getElementById('go').addEventListener('click', onClick);
